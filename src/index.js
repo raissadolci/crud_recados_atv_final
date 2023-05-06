96120981de55511b1b37de1d56026adfd3f920f8
Raissa Dolci <raissadolcci@gmail.com>
2023-05-06 14:30:07 -0400

    CRUD de usuarios sem validações

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express, { response } from "express";
+import express, { request, response } from "express";
 
 const servidor = express();
 
@@ -13,16 +13,47 @@ servidor.post('/usuarios', (request, response)=>{
     const usuario = request.body;
 
     usuarios.push({
-        id:Math.floor(Math.random()*234), nome:usuario.nome, email:usuario.email, senha:usuario.senha
+        id:Math.floor(Math.random()*234), 
+        nome:usuario.nome, 
+        email:usuario.email, 
+        senha:usuario.senha
     });
     response.status(201).json("Usuário criado com sucesso");
 })
 
-//Leitura dos usuários
+//Leitura de todos os usuários
 servidor.get('/usuarios', (request, response)=>{
     response.status(200).json(usuarios)
 })
 
 servidor.listen(3030, ()=>console.log("Servidor rodando"))
 
+//Leitura de um usuário por ID
+servidor.get('/usuarios/:id', (request, response)=>{
+    const id = Number(request.params.id);
+    const usuario = usuarios.find(usuario => usuario.id===id);
+    response.json(usuario);
+})
 
+//Atualizar usuário por ID
+servidor.put('/usuarios/:id', (request, response)=>{
+    const usuario = request.body;
+    const id = Number(request.params.id);
+    const indexUsuario = usuarios.findIndex(usuario => usuario.id===id);
+    usuarios[indexUsuario] = {
+        id: id,
+        nome: usuario.nome,
+        email: usuario.email,
+        senha: usuario.senha
+    };
+
+    response.status(200).json(usuarios[indexUsuario]);
+})
+
+//Deletar usuário por ID
+servidor.delete('/usuarios/:id', (request, response)=>{
+    const id = Number(request.params.id);
+    const indexUsuario = usuarios.findIndex(usuario => usuario.id === id);
+    usuarios.splice(indexUsuario, 1);
+    return response.status(200).json();
+})
